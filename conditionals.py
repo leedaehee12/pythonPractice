ab45fddf00d4cb12291ff7f273a6c85c1a285281
sprinter239@gmail.com <sprinter239@gmail.com>
2024-08-08 19:31:52 +0900

    add conditionals.py and loops.py

---
+++ conditionals.py
@@ -0,0 +1,12 @@
+#conditionals.py
+
+score = 58
+
+if score >= 90 :
+    print("A")
+elif score >= 80 :
+    print("B")
+elif score >= 70 :
+    print("C")
+else:
+    print("F")
---
+++ loops.py
@@ -0,0 +1,12 @@
+#loops.py
+
+#for 반복문
+for i in range(5):
+    print("for loop iteration", i)
+
+
+#while 반복문
+count = 0
+while count < 5:
+    print("while loop iteration", count)
+    count += 1
